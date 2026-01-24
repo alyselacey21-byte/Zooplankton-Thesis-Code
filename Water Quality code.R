aae24f96d4fe2f17bc22394d9fdb494350fd44d0
A. L. <alyselacey21@gmail.com>
2026-01-23 20:03:31 -0800

    Water quality summary stats and CSV upload

---
+++ Water Quality code.R
@@ -0,0 +1,11 @@
+WaterQuality <- read.csv("C:/Users/al-la/Downloads/SMSCG_wq_data_2017-2023_clean_tzone.csv")
+head(WaterQuality)
+tail(WaterQuality)
+nchar(WaterQuality)
+class(WaterQuality)
+typeof(WaterQuality)
+str(WaterQuality)
+
+
+library(tidyverse)
+library(mgcv)
