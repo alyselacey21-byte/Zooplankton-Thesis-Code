afaff9ede1f55dbc1c455becfbed7255e4ba125c
A. L. <alyselacey21@gmail.com>
2026-03-02 11:38:58 -0800

    Graphs of 6 out of 7 trawls and the years they collected data

--- Graphs per Trawl.R
+++ Graphs per Trawl.R
@@ -54,3 +54,44 @@ unlink(infile1)
 
 
 
+Directed_Outflow_Project_Lower_Trophic_Study <- subset(dt1, Source == "DOP")
+
+#For some reason this is not coming up/does not exist in the data I have brought in. May want to bring it in individually and merge dt1 with the new data.
+Yolo_Bypass_Fish_Monitoring_Program <- subset(dt1, Source == "YBFMP")
+
+Fish_Restoration_Program <- subset(dt1, Source == "FRP")
+
+Environmental_Monitoring_Program <- subset(dt1, Source == "EMP")
+
+Summer_Townet_Survey <- subset(dt1, Source == "STN")
+
+Twenty_mm_survey <- subset(dt1, Source == "20mm")
+
+Fall_Midwater_Trawl <- subset(dt1, Source == "FMWT")
+
+
+#histograms of source and year
+
+ggplot(Fish_Restoration_Program, aes(x = Year)) +
+  geom_bar(binwidth = 0.5, fill = "blue", color = "black") +
+  labs(title = "Years that the Fish Restoration Program Collected Data", x = "Year", y = "Count")
+
+ggplot(Directed_Outflow_Project_Lower_Trophic_Study, aes(x = Year)) +
+  geom_bar(binwidth = 0.5, fill = "green", color = "black") +
+  labs(title = "Years that the Directed Outflow Project Lower Trophic Study Collected Data", x = "Year", y = "Count")
+
+ggplot(Environmental_Monitoring_Program, aes(x = Year)) +
+  geom_bar(binwidth = 0.5, fill = "purple", color = "black") +
+  labs(title = "Years that the Environmental Monitoring Program Collected Data", x = "Year", y = "Count")
+
+ggplot(Summer_Townet_Survey, aes(x = Year)) +
+  geom_bar(binwidth = 0.5, fill = "yellow", color = "black") +
+  labs(title = "Years that the Summer Townet Survey Collected Data", x = "Year", y = "Count")
+
+ggplot(Twenty_mm_survey, aes(x = Year)) +
+  geom_bar(binwidth = 0.5, fill = "steelblue", color = "black") +
+  labs(title = "Years that the 20 mm survey Collected Data", x = "Year", y = "Count")
+
+ggplot(Fall_Midwater_Trawl, aes(x = Year)) +
+  geom_bar(binwidth = 0.5, fill = "darkgreen", color = "black") +
+  labs(title = "Years that the Fall Midwater Trawl Collected Data", x = "Year", y = "Count")
