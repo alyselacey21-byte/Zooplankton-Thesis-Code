ab9f70a1c7c63df1aa641c4b4e0f4ffcf8a6be41
A. L. <alyselacey21@gmail.com>
2026-03-02 11:41:32 -0800

    Graphs of rotifers

---
+++ Graphs of rotifers.R
@@ -0,0 +1,52 @@
+library(tidyverse)
+library(mgcv)
+
+options(HTTPUserAgent="EDI_CodeGen")
+
+
+inUrl1  <- "https://pasta.lternet.edu/package/data/eml/edi/539/4/58dd1dde8e38614a9cc48794f527bdec" 
+infile1 <- tempfile()
+try(download.file(inUrl1,infile1,method="curl",extra=paste0(' -A "',getOption("HTTPUserAgent"),'"')))
+if (is.na(file.size(infile1))) download.file(inUrl1,infile1,method="auto")
+
+dt1 <-read.csv(infile1,header=F 
+               ,skip=1
+               ,sep=","  
+               ,quot='"' 
+               , col.names=c(
+                 "Source",     
+                 "Station",     
+                 "Latitude",     
+                 "Longitude",     
+                 "Year",     
+                 "Date",     
+                 "Datetime",     
+                 "SampleID",     
+                 "TowType",     
+                 "AmphipodCode",     
+                 "Tide",     
+                 "BottomDepth",     
+                 "Chl",     
+                 "Secchi",     
+                 "Temperature",     
+                 "Turbidity",     
+                 "Microcystis",     
+                 "pH",     
+                 "DO",     
+                 "SalSurf",     
+                 "SalBott",     
+                 "SizeClass",     
+                 "Volume",     
+                 "Phylum",     
+                 "Class",     
+                 "Order",     
+                 "Family",     
+                 "Genus",     
+                 "Species",     
+                 "Taxname",     
+                 "Lifestage",     
+                 "Taxlifestage",     
+                 "CPUE",     
+                 "Undersampled"    ), check.names=TRUE)
+
+unlink(infile1)
