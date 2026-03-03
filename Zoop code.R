246440f7b191e4c50117bce8d6a158538be17d2d
Alyse Laurel Lacey <alacey@ucdavis.edu>
2026-03-03 08:51:04 -0800

    updates

--- Zoop code.R
+++ Zoop code.R
@@ -5,3 +5,5 @@ inUrl1  <- "https://pasta.lternet.edu/package/data/eml/edi/539/4/58dd1dde8e38614
 infile1 <- tempfile()
 try(download.file(inUrl1,infile1,method="curl",extra=paste0(' -A "',getOption("HTTPUserAgent"),'"')))
 if (is.na(file.size(infile1))) download.file(inUrl1,infile1,method="auto")
+
+head(infile1)
