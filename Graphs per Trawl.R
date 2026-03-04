68360fd79c1b7a4311b7667a32ffd2c7699a62bf
A. L. <alyselacey21@gmail.com>
2026-03-04 09:19:53 -0800

    Updated previous histograms by combining them. Made a new graph that shows the seasons that trawls collect data by month, year, and amount.

--- Graphs per Trawl.R
+++ Graphs per Trawl.R
@@ -52,8 +52,37 @@ dt1 <-read.csv(infile1,header=F
 
 unlink(infile1)
 
+#Here I am attempting to remake the below histograms and graph it based on trawls per DATE not per line.
+
+dt1 %>%
+  mutate(Date = as.Date(Date),
+         Year = year(Date),
+         MonthDay = format(Date, "%m-%d")) %>%
+  distinct(Source, Year, MonthDay) %>%
+  ggplot(aes(x = MonthDay, y = factor(Year), fill = Source)) +
+  geom_tile(color = "white", linewidth = 0.3) +
+  scale_x_discrete(breaks = c("01-01", "02-01", "03-01", "04-01", "05-01",
+                              "06-01", "07-01", "08-01", "09-01", "10-01",
+                              "11-01", "12-01"),
+                   labels = month.abb) +
+  labs(
+    title = "Sampling Dates by Year and Trawl Source",
+    x = "Date",
+    y = "Year",
+    fill = "Trawl Source"
+  ) +
+  theme_bw() +
+  theme(
+    axis.text.x = element_text(angle = 45, hjust = 1),
+    panel.grid = element_blank()
+  )
 
 
+
+
+
+
+#these histograms and objects are for total per line observations, not for per day/date observations. Each line has one type of Zoop so it inflates the number of observations depending on the amount of zoops recorded. 
 Directed_Outflow_Project_Lower_Trophic_Study <- subset(dt1, Source == "DOP")
 
 #For some reason this is not coming up/does not exist in the data I have brought in. May want to bring it in individually and merge dt1 with the new data.
@@ -68,6 +97,43 @@ Summer_Townet_Survey <- subset(dt1, Source == "STN")
 Twenty_mm_survey <- subset(dt1, Source == "20mm")
 
 Fall_Midwater_Trawl <- subset(dt1, Source == "FMWT")
+
+#Combined all the lower histograms with facetwrap to make one image that is easier to read. 
+combined <- bind_rows(
+  Fish_Restoration_Program %>% mutate(Source = "Fish Restoration Program"),
+  Directed_Outflow_Project_Lower_Trophic_Study %>% mutate(Source = "Directed Outflow Project"),
+  Environmental_Monitoring_Program %>% mutate(Source = "Environmental Monitoring Program"),
+  Summer_Townet_Survey %>% mutate(Source = "Summer Townet Survey"),
+  Twenty_mm_survey %>% mutate(Source = "20 mm Survey"),
+  Fall_Midwater_Trawl %>% mutate(Source = "Fall Midwater Trawl")
+)
+
+# Define colors per source
+source_colors <- c(
+  "Fish Restoration Program"          = "blue",
+  "Directed Outflow Project"          = "green",
+  "Environmental Monitoring Program"  = "purple",
+  "Summer Townet Survey"              = "yellow",
+  "20 mm Survey"                      = "steelblue",
+  "Fall Midwater Trawl"               = "darkgreen"
+)
+
+# Plot
+ggplot(combined, aes(x = Year, fill = Source)) +
+  geom_bar(color = "black") +
+  scale_fill_manual(values = source_colors) +
+  facet_wrap(~ Source, scales = "free_y") +
+  labs(
+    title = "Years Each Trawl Survey Collected Data",
+    x = "Year",
+    y = "Count"
+  ) +
+  theme_bw() +
+  theme(
+    legend.position = "none",  # legend redundant since facet labels show source
+    strip.text = element_text(face = "bold", size = 8),
+    axis.text.x = element_text(angle = 45, hjust = 1)
+  )
 
 
 #histograms of source and year
